e05ae171d41c93e8eb17428a8dbc68e2e912cee6
Nardin Nader <nardinnader123@gmail.com>
2020-07-17 20:09:45 +0200

    Password

--- Driver_ADC/main.c
+++ Driver_ADC/main.c
@@ -5,26 +5,69 @@
  *      Author: Meriam
  */
 
+
 #define F_CPU  16000000
-#include "Temp_Sensor.h"
 #include "LCD.h"
+#include "Keypad.h"
+#include "Temp_Sensor.h"
 #include <util/delay.h>
 
-int main (void){
+#define SIZE 4
+//#define NULL (void*(0))
+
+int main (void) {
 
 
-	Uint16 Temp = 0;
+	const Uint8 PassWord[] ={1 , 2 , 3 , 4 } ;
+	Uint8 INPUT_PASS[SIZE] ;
+	Uint8 i =0;
+	Uint8 Flag = 0;
+	Uint8 Temp = 0;
 
 	LCD_initial();
-	TempSensor_Initial();
+	KEYPAD_Initial ();
+    TempSensor_Initial();
 
-	while(1){
+	while(1)
+{
+		for (i = 0 ; i < SIZE ; i++)
+			{
 
-		TempSensor_Read(&Temp);
+			INPUT_PASS[i] = KEYPAD_GetValue();
 
-		 LCD_Clear();
+				if(INPUT_PASS[i])
+				{
+					LCD_WriteChar('*');
+				}
 
-		LCD_WriteInteger(Temp);
+			}
 
-	}
+		for (i=0 ; i <SIZE  ; i++)
+		{
+			if(INPUT_PASS[i] == PassWord[i])
+			Flag = 0 ;
+			else
+			{
+				 Flag= 1;
+	         	 LCD_Clear ();
+				 Uint8 Incorrect_Case[] = "incorrect";
+				 LCD_WriteString(Incorrect_Case);
+				 LCD_Clear ();
+				 break;
+			}
+		}
+		 if(Flag == 0){
+			 LCD_Clear ();
+			 Uint8 Correct_Case[] = "WELCOME!";
+			 LCD_WriteString(Correct_Case);
+			 TempSensor_Read(&Temp);
+			 LCD_Clear();
+			 LCD_WriteInteger(Temp);
+		 }
+
+
+
+
 }
+}
+
